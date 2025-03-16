252fa50238afd37fd756de57c23d6764a5a2f2a4
yas-cm <yasminhayashi234@gmail.com>
2025-03-16 14:32:48 -0300

    Formativa Python finalizada e funcionando!

--- 5° Período/TAD/FormtaivaPython.py
+++ 5° Período/TAD/FormtaivaPython.py
@@ -9,23 +9,47 @@ class N_Racional:
         else:
             print('ERRO! DIVISAO POR 0')
             return 0
+
+    def N_Rac_simplifica(self):
+        divisoresd = []
+        divisoresn = [] 
+        for i in range (self._d,0,-1):
+            if (self._d % i == 0):
+                divisoresd.append(self._d/i)
+
+        for j in range (self._n,0,-1):
+            if (self._n % j == 0):
+                divisoresn.append(self._n/j)
+        
+        
+        for elemento in divisoresd[::-1]:
+            if elemento in divisoresn:
+                self._d = self._d/elemento
+                self._n = self._n/elemento
+                
+                return self
     
     def __add__(self, q):
         valor = N_Racional()
         valor._d = self._d * q._d
-        valor._n = (self._n * valor._d) + (q._n * valor._d)
+        valor._n = round((self._n * valor._d/self._d) + (q._n * valor._d/q._d))
+        valor.N_Rac_simplifica()
         return valor
     
     def __sub__(self, q):
         valor = N_Racional()
         valor._d = self._d * q._d
-        valor._n = (self._n * valor._d) - (q._n * valor._d)
+        valor._n = round((self._n * valor._d/self._d) - (q._n * valor._d/q._d))
+        if(valor._n == 0):
+            valor._d = 1
+        valor.N_Rac_simplifica()
         return valor
     
     def __mul__(self, q):
         valor = N_Racional()
         valor._d = self._d * q._d
         valor._n = self._n * q._n
+        valor.N_Rac_simplifica()
         return valor
     
     def __truediv__(self, q):
@@ -33,16 +57,16 @@ class N_Racional:
         if(self._d != 0 and q._d != 0):
             valor._d = self._d*q._n
             valor._n = self._n*q._d
+            valor.N_Rac_simplifica()
             return valor
         else:
             print('ERRO! DIVISAO POR 0')
             return 0
     
-    # def N_Rac_simplifica(self):
-    #     return
+    
     
     def __str__(self):
-        return f'{self._n}/{self._d}'
+        return f'{int(self._n)}/{int(self._d)}'
 
 ### main ###
 q1 = N_Racional(1,3)
